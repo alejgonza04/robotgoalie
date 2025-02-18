dcdb86e366bd8fc5037a56d5293624aabbc6ccbe
jesusgil7 <32800172+jesusgil7@users.noreply.github.com>
2025-02-17 19:31:33 -0500

    Update Motor_Arduino.ino
    Right and left implementation

--- Motor_Arduino.ino
+++ Motor_Arduino.ino
@@ -3,14 +3,13 @@
 #include <Servo.h>
 
 Servo servo;  // Initialize a Servo object to manage a servo
+int position = 90;  // Start at center (90°)
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  // initialize the pushbutton pin as a pull-up input
-  pinMode(BUTTON_PIN7, INPUT_PULLUP);
-  pinMode(BUTTON_PIN4, INPUT_PULLUP);
+  Serial.begin(115200);
   servo.attach(5);  // Connects the servo on pin 9 to the servo object
   servo.write(0);   // Moves the servo to 0 degrees immediately upon startup
+  Serial.println("Enter a value:");
 }
 
 void loop() {
@@ -18,28 +17,28 @@ void loop() {
   int button_state1 = digitalRead(BUTTON_PIN7);
   int button_state2 = digitalRead(BUTTON_PIN4);
   int counter_clockwise = 1;
-
-  if (button_state1 == 0){
     
-    for (int pos = 0; pos <= 180; pos += 1) {  // Gradually move the servo from 0 to 180 degrees
-    servo.write(pos);  // Set servo position to 'pos' degrees
-    if (pos == 180){
-      pos = 0;
+
+    if (Serial.available() > 0) {  // Check if data is available
+      String input = Serial.readStringUntil('\n');  // Read input until newline
+      input.trim();
+      Serial.println(input);
+      if(input.equals("RIGHT")){
+        Serial.println("Rotating Right");
+        for (; position <= 180; position++) {  // Gradually move the servo from 0 to 180 degrees
+          servo.write(position);  // Set servo position to 'pos' degrees
+          delay(10);
+        }
+      }
+      else if (input.equals("LEFT")) {  // Move left
+        Serial.println("Rotating Left");
+        for (; position >= 0; position--) {
+          servo.write(position);
+          delay(10);
+          }
+      }
     }
-    if (button_state1 == )         // Delay 10ms to allow the servo to reach the new position
-  }
-    counter_clockwise = 0;
-    Serial.println("Button 1");
-  }
-  else if (button_state2 == 0){
-    for (int pos = 180; pos >= 0; pos -= 1) {  // Gradually move the servo from 0 to 180 degrees
-    servo.write(pos);  // Set servo position to 'pos' degrees
-    if (pos == 0){
-      pos = 180;
-    }   
-    Serial.println("Button 2");
-  }
-  }
 
   delay(100);
+
 }
